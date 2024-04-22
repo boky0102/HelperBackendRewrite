acf2ee5ccb4e5374cc2a7e02987e1190b158073b
boky0102 <bivankovi@protonmail.com>
2024-04-22 22:13:32 +0200

    Initialized new Spring Boot project

---
+++ src/main/java/com/example/helperbackend/HelperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.helperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelperBackendApplication.class, args);
+	}
+
+}
